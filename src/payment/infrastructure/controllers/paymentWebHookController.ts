bfcff9cadef860a4a83404c3a855c2bdafbab59e
michaeltorrico07 <michaeltorrico1804@gmail.com>
2025-06-26 23:17:12 -0300

    unsa a los geis

--- src/payment/infrastructure/controllers/paymentWebHookController.ts
+++ src/payment/infrastructure/controllers/paymentWebHookController.ts
@@ -2,7 +2,18 @@ import { Response, Request } from 'express'
 
 export class PaymentWebHookController {
   handleWebHook = async (req: Request, res: Response): Promise<void> => {
-    console.log(req)
+    const { type, data, action } = req.body
+    console.log(req.body)
+    switch (type) {
+      case 'payment':
+        console.log(data, action)
+        break
+      case 'chargebacks':
+        break
+      default:
+        console.log('unsa a los geis')
+        break
+    }
     console.log('webhook')
   }
 }
